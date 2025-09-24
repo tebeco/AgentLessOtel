ff63de6f228e64580a3016e9f7c0d710fba27c33
TeBeCo <tebeco@gmail.com>
2025-09-24 21:29:15 +0400

    Add BackgroundService with log and external http call

--- AgentLessOtelDataDog/Program.cs
+++ AgentLessOtelDataDog/Program.cs
@@ -1,14 +1,36 @@
+using AgentLessOtelDataDog;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.Mime;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Minimal repro code for testing
+// * endpoint (traces + log correlation)
+// * external http call (external traces correlation)
+// * background service (traces + external traces + log correlation)
 builder.Services.AddHealthChecks().AddCheck("self", () => HealthCheckResult.Healthy(), ["live"]);
+builder.Services.AddHostedService<MyBackgroundService>();
+builder.Services.AddHttpClient<SelfHttpClient>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5555");
+});
 
 var app = builder.Build();
 
-app.MapHealthChecks("/", new HealthCheckOptions
+app.MapGet("/", async ([FromServices] SelfHttpClient selfHttpClient) =>
 {
-    Predicate = r => r.Tags.Contains("live")
+    var livenessResult = await selfHttpClient.GetLivenesstAsync();
+    var readinessResult = await selfHttpClient.GetReadinesstAsync();
+    return TypedResults.Text($"""
+        <ul>
+            <li>livenessResult: {livenessResult}</li>
+            <li>readinessResult: {readinessResult}</li>
+        </ul>
+        """, MediaTypeNames.Text.Html);
 });
+app.MapHealthChecks("/health");
+app.MapHealthChecks("/live", new HealthCheckOptions { Predicate = r => r.Tags.Contains("live") });
 
 app.Run();
---
+++ AgentLessOtelDataDog/SelfHttpClient.cs
@@ -0,0 +1,19 @@
+namespace AgentLessOtelDataDog;
+
+public class SelfHttpClient(HttpClient httpClient)
+{
+    public async Task<string> GetRootAsync()
+    {
+        return await httpClient.GetStringAsync("/");
+    }
+
+    public async Task<string> GetReadinesstAsync()
+    {
+        return await httpClient.GetStringAsync("/health");
+    }
+
+    public async Task<string> GetLivenesstAsync()
+    {
+        return await httpClient.GetStringAsync("/live");
+    }
+}
