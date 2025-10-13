644231c6e4beb5f076451f6fc2b302d0f9f8c92c
TeBeCo <tebeco@gmail.com>
2025-10-14 00:40:02 +0400

    remove DD missleading comment

--- src/MyNuget.Telemetry/InfrastructureOptions.cs
+++ src/MyNuget.Telemetry/InfrastructureOptions.cs
@@ -4,13 +4,10 @@ public class InfrastructureOptions
 {
     public const string SectionName = "Infrastructure";
 
-    // ideally this should be understood by the datadog as what DD_ENV env var but properly as code
     public string Team { get; set; } = "blabla";
 
-    // ideally this should be understood by the datadog as what DD_ENV env var but properly as code
     public string Environment { get; set; } = "local";
 
-    // ideally this should be understood by the datadog as what DD_SERVICE env var but properly as code
     public string Service { get; set; } = "nominal-api";
 
     public string Version { get; set; } = "1.2.3";
