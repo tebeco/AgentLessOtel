bb17c5a01c8fce666645285dbb167d0e0b28f962
TeBeCo <TeBeCo@gmail.com>
2025-10-13 21:16:00 +0400

    MongoDb driver are still bad enough to not work on their own with OTEL

--- src/MyNuget.Mongo/MongoServiceCollectionExtensions.cs
+++ src/MyNuget.Mongo/MongoServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.Configuration;
+using MongoDB.Driver.Core.Extensions.DiagnosticSources;
 using MyNuget.Mongo;
 
 #pragma warning disable IDE0130 // Namespace does not match folder structure
@@ -26,8 +27,12 @@ public static class MongoServiceCollectionExtensions
             {
                 var options = sp.GetRequiredService<IOptions<MyMongoDbOptions>>().Value;
 
+                var instrumentationOptions = new InstrumentationOptions { CaptureCommandText = true };
+                var diagnosticsActivityEventSubscriber = new DiagnosticsActivityEventSubscriber(instrumentationOptions);
+
                 var settings = MongoClientSettings.FromConnectionString(options.ConnectionString);
                 settings.LoggingSettings = new LoggingSettings(sp.GetRequiredService<ILoggerFactory>());
+                settings.ClusterConfigurator = cb => cb.Subscribe(diagnosticsActivityEventSubscriber);
 
                 return new MongoClient(settings);
             });
@@ -45,7 +50,7 @@ public static class MongoServiceCollectionExtensions
         builder
             .Services
             .AddOpenTelemetry()
-            .WithTracing(tracer => tracer.AddSource(ActivityNameSource));
+            .WithTracing(tracer => tracer.AddSource(DiagnosticsActivityEventSubscriber.ActivitySourceName));
 
         return builder;
     }
